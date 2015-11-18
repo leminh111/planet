d5c72682772494aaf0639aef394e1150b934e089
leminh111 <leminh111@gmail.com>
2015-11-18 15:25:30 +0700

    setup gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('dist', function() {
+  var b = browserify({
+    entries: './src/scripts/index.js',
+    debug: true,
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./www/assets/js/'))
+});
+
+gulp.task('watch', ['dist'], function() {
+  gulp.watch('./src/scripts/**', ['dist'])
+})
