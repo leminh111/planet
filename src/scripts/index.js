55195df3ca8040ce172f76f4668d4a97d25fe9cb
soyelmnd <minhnd.it@gmail.com>
2015-12-01 15:21:48 +0700

    set .fluid fullsize
    function setfullsize for div and canvas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -35,3 +35,26 @@ document.addEventListener('DOMContentLoaded', function() {
   .setDeltaT(0.02)
   .bigbang();
 }, false)
+
+// on event resize and load
+// find all .fluid and set width height attribute for each
+
+function setFullsize() {
+  var els = document.getElementsByClassName('fluid');
+  for (var i=0; i<els.length; i++) {
+    var el = els[i];
+
+    // Canvas need width&height attr so the ratio remains 1:1
+    if (el.nodeName == 'CANVAS') {
+      el.setAttribute('width', window.innerWidth);
+      el.setAttribute('height', window.innerHeight);
+    }
+
+    el.style.width = window.innerWidth + 'px';
+    el.style.height = window.innerHeight + 'px';
+
+  }
+}
+
+window.addEventListener('resize', setFullsize, false);
+document.addEventListener('DOMContentLoaded', setFullsize, false);
